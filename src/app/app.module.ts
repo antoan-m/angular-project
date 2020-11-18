3133a15564d67686e09f85189b1d479c14c46ea0
antoan-m <62904998+antoan-m@users.noreply.github.com>
2020-11-18 23:49:56 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { GameComponent } from './game/game.component';
+import { GamesListComponent } from './games-list/games-list.component';
+import { ProfileComponent } from './profile/profile.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { GameAddComponent } from './game-add/game-add.component';
+import { UserOrdersComponent } from './user-orders/user-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    GameComponent,
+    GamesListComponent,
+    ProfileComponent,
+    WishlistComponent,
+    GameAddComponent,
+    UserOrdersComponent
   ],
   imports: [
     BrowserModule,
