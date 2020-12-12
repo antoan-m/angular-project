352fc0e0aa5ad6b6c5629f05f96e615926b7ed7d
antoan-m <62904998+antoan-m@users.noreply.github.com>
2020-12-12 18:17:05 +0200

    docs folder

---
+++ docs/assets/js/scripts.js
@@ -0,0 +1,25 @@
+//sticky header
+window.onscroll = function() {
+    if (document.body.scrollTop > 158 || document.documentElement.scrollTop > 158) {
+        document.getElementById("header").style.top = "-158px";
+        document.getElementById("header-sticky").style.display = "block";
+        document.getElementById("header-sticky").style.top = "0px";
+        // document.getElementById("body").style.margin = "0";
+        document.getElementById("search-results-table").style.margin = "134px 0 0 0";
+    } else {
+        document.getElementById("header").style.top = "0px";
+        //document.getElementById("header-sticky").style.display = "none";
+        document.getElementById("header-sticky").style.top = "-50px";
+        // document.getElementById("body").style.margin = "158px 0 0 0";
+        document.getElementById("search-results-table").style.margin = "26px 0 0 0";
+    }
+}
+
+//close search results on outside click
+window.onclick = function() {
+    this.search_form.reset();
+}
+
+window.onclick = function() {
+    this.search_form_sticky.reset();
+}
